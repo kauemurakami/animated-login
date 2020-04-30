a0e24fd424591d7eade1c1f1c143c583a532ebb5
Kauê Murakami <kauetmurakami@gmail.com>
2020-04-29 21:10:17 -0300

    animando tela de login

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+import 'package:flutter/scheduler.dart' show timeDilation;
 import 'package:animatedlogin/input_custom.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -7,7 +9,50 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home>  with SingleTickerProviderStateMixin{
+
+  AnimationController _animationController;
+  Animation<double> _animacaoBlur;
+  Animation<double> _animacaoFade;
+  Animation<double> _animacaoSize;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _animationController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this
+    );
+    _animacaoBlur = Tween<double>(
+      begin: 5,
+      end: 0,
+    ).animate(CurvedAnimation(
+        parent: _animationController,
+      curve: Curves.ease
+      ));
+    _animacaoFade = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeInOutQuint
+    ));
+    _animacaoSize = Tween<double>(
+      begin: 0,
+      end: 500,
+    ).animate(CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.decelerate
+    ));
+    _animationController.forward();
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _animationController.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,94 +61,126 @@ class _HomeState extends State<Home> {
         child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-              Container(
-              height: 400,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("images/fundo.png"),
-                fit: BoxFit.fill
-              )
-            ),
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                     left: 10,
-                     child: Image.asset("images/detalhe1,png"),
-                    ),
-                    Positioned(
-                      left: 50,
-                      child: Image.asset("images/detalhe2,png"),
-                    ),
-                  ],
-                ),
+              AnimatedBuilder(
+                animation: _animacaoBlur,
+                builder: (context, widget){
+                  return Container(
+                      height: 400,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("images/fundo.png"),
+                              fit: BoxFit.fill
+                          )
+                      ),
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(
+                          sigmaY: _animacaoBlur.value,
+                          sigmaX: _animacaoBlur.value,
+                        ),
+                        child: Stack(
+                          children: <Widget>[
+                            Positioned(
+                              left: 10,
+                              child: FadeTransition(
+                                opacity: _animacaoFade,
+                                child: Image.asset("images/detalhe1.png"),
+                              ),
+                            ),
+                            Positioned(
+                              left: 50,
+                              child: FadeTransition(
+                                opacity: _animacaoFade,
+                                child: Image.asset("images/detalhe2.png")
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                  );
+                },
               ),
               Padding(
                 padding: EdgeInsets.only(left: 30, right: 30),
                 child: Column(
                   children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(8),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey[200],
-                          blurRadius: 15,
-                          spreadRadius: 4
-                        )
-                      ]
-                    ),
-                      child: Column(
-                        children: <Widget>[
-                          InputCustom(
-                              hint: 'Email',
-                              obscure: false,
-                              icone : Icon(Icons.person)
+                    AnimatedBuilder(
+                      animation: _animacaoSize,
+                      builder: (context, widget){
+                        return Container(
+                          width: _animacaoSize.value,
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Colors.grey[200],
+                                    blurRadius: 15,
+                                    spreadRadius: 4
+                                )
+                              ]
                           ),
-                          InputCustom(
-                              hint: 'Senha',
-                              obscure: true,
-                              icone : Icon(Icons.lock)
+                          child: Column(
+                            children: <Widget>[
+                              InputCustom(
+                                  hint: 'Email',
+                                  obscure: false,
+                                  icone : Icon(Icons.person)
+                              ),
+                              InputCustom(
+                                  hint: 'Senha',
+                                  obscure: true,
+                                  icone : Icon(Icons.lock)
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
+                        );
+                      },
                     ),
                     SizedBox(height: 20,),
-                    InkWell(
-                      onTap: (){
+                    AnimatedBuilder(
+                      animation: _animacaoSize,
+                      builder: (context,widget){
+                        return InkWell(
+                          onTap: (){
 
-                      },
-                      child: Container(
-                        height: 50,
-                        child: Center(
-                          child: Text(
-                              "Entrar",
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold
+                          },
+                          child: Container(
+                            height: 50,
+                            width: _animacaoSize.value,
+                            child: Center(
+                              child: Text(
+                                "Entrar",
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold
+                                ),
+                              ),
+                            ),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                gradient: LinearGradient(
+                                    colors: [
+                                      Color.fromRGBO(255, 100, 127, 1),
+                                      Color.fromRGBO(255, 123, 145, 1),
+                                    ]
+                                )
                             ),
                           ),
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          gradient: LinearGradient(
-                            colors: [
-                              Color.fromRGBO(255, 100, 127, 1),
-                              Color.fromRGBO(255, 123, 145, 1),
-                            ]
-                          )
-                        ),
-                      ),
+                        );
+                      },
                     ),
                     SizedBox(height: 10,),
-                    Text(
-                      "Esqueci minha senha",
-                      style: TextStyle(
-                        color:Color.fromRGBO(255, 100, 127, 1),
-                      fontWeight: FontWeight.bold,
-                      )
+                    FadeTransition(
+                      opacity: _animacaoFade,
+                      child: Text(
+                          "Esqueci minha senha",
+                          style: TextStyle(
+                            color:Color.fromRGBO(255, 100, 127, 1),
+                            fontWeight: FontWeight.bold,
+                          )
+                      ),
                     )
                   ],
                 ),
